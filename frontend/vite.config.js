4449b0e5b1e7bf6cf84b89acce7cdc7158e27399
Cxze <93561762+cxze299@users.noreply.github.com>
2026-08-26 07:27:19 +0800

    chore: 支持远程接口联调

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://127.0.0.1:8080',
+	  '/api': process.env.AGP_DEV_API_TARGET || 'http://127.0.0.1:8080',
     },
   },
 });
